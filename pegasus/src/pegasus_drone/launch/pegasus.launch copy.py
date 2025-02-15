5b2684d05a9cbe17ff7d94a24e57911d45e4b592
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2025-02-15 18:56:58 +0000

    feat: scripts and launch files for the pegasus drone

---
+++ pegasus/src/pegasus_drone/launch/pegasus.launch copy.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+import os
+import sys
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    
+    # --------------------------------
+    # Define the vehicle ID
+    # --------------------------------
+    
+    # Set the default vehicle id (note: this is a trick due to the parameter reading limitation in ROS2)
+    default_vehicle_id = 1
+    vehicle_id = default_vehicle_id
+    for arg in sys.argv:
+        if arg.startswith('vehicle_id:='):
+            vehicle_id = int(arg.split(':=')[1])
+    
+    # ----------------------------------------
+    # ---- DECLARE THE LAUNCH ARGUMENTS ------
+    # ----------------------------------------
+
+    # Namespace and ID of the vehicle as parameter received by the launch file
+    id_arg = DeclareLaunchArgument('vehicle_id', default_value=str(vehicle_id), description='Drone ID in the network')
+    namespace_arg = DeclareLaunchArgument('vehicle_ns', default_value='drone', description='Namespace to append to every topic and node name')
+
+    drone_params_file_arg = DeclareLaunchArgument(
+        'drone_params', 
+        default_value=os.path.join(get_package_share_directory('pegasus_drone'), 'config', 'pegasus.yaml'),
+        description='The directory where the drone parameters such as mass, thrust curve, etc. are defined')
+
+    """Launch file which brings up visual slam node configured for RealSense."""
+    realsense_camera_node = Node(
+        name='camera',
+        namespace=[
+            LaunchConfiguration('vehicle_ns'), 
+            LaunchConfiguration('vehicle_id')],
+        package='realsense2_camera',
+        executable='realsense2_camera_node',
+        #plugin='realsense2_camera::RealSenseNodeFactory',
+        parameters=[LaunchConfiguration('drone_params')],
+        #extra_arguments=[{'use_intra_process_comms': True}],
+    )
+
+    visual_slam_node = ComposableNode(
+        name='visual_slam_node',
+        namespace=[
+            LaunchConfiguration('vehicle_ns'), 
+            LaunchConfiguration('vehicle_id')],
+        package='isaac_ros_visual_slam',
+        plugin='nvidia::isaac_ros::visual_slam::VisualSlamNode',
+        parameters=[{
+            'enable_image_denoising': False,
+            'rectified_images': True,
+            'enable_imu_fusion': True,
+            'gyro_noise_density': 0.000244,
+            'gyro_random_walk': 0.000019393,
+            'accel_noise_density': 0.001862,
+            'accel_random_walk': 0.003,
+            'calibration_frequency': 200.0,
+            'image_jitter_threshold_ms': 22.00,
+            'base_frame': 'camera_link',
+            'imu_frame': 'camera_gyro_opt ical_frame',
+            'enable_slam_visualization': True,
+            'enable_landmarks_view': True,
+            'enable_observations_view': True,
+            'camera_optical_frames': [
+                'camera_infra1_optical_frame',
+                'camera_infra2_optical_frame',
+            ],
+        }],
+        remappings=[
+            ('visual_slam/image_0', 'camera/infra1/image_rect_raw'),
+            ('visual_slam/camera_info_0', 'camera/infra1/camera_info'),
+            ('visual_slam/image_1', 'camera/infra2/image_rect_raw'),
+            ('visual_slam/camera_info_1', 'camera/infra2/camera_info'),
+            ('visual_slam/imu', 'camera/imu'),
+        ],
+    )
+
+    visual_slam_launch_container = ComposableNodeContainer(
+        name='visual_slam_launch_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[visual_slam_node], 
+        output='screen',
+        emulate_tty=True, # needed for display of logs
+        arguments=['--ros-args', '--log-level', 'info'],
+    )
+
+    # ----------------------------------------
+    # ---- RETURN THE LAUNCH DESCRIPTION -----
+    # ----------------------------------------
+    return LaunchDescription([
+        # Launch arguments
+        id_arg, 
+        namespace_arg, 
+        drone_params_file_arg,
+        # Nodes
+        visual_slam_launch_container, 
+        realsense_camera_node
+    ])
