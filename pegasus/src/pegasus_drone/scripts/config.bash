f286d8dbf04966e9c579e31acd7506283c14d178
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2025-02-16 19:27:50 +0000

    feat: script to make sure that wifi is working at full power

--- pegasus/src/pegasus_drone/scripts/config.bash
+++ pegasus/src/pegasus_drone/scripts/config.bash
@@ -5,6 +5,11 @@ sudo cp services/jetson_clocks.service /etc/systemd/system/jetson_clocks.service
 sudo systemctl daemon-reload
 sudo systemctl enable jetson_clocks.service
 
+# Create a service that automatically removes the power-saving mode from the wifi card
+sudo cp services/wifi_full.service /etc/systemd/system/wifi_full.service
+sudo systemctl daemon-reload
+sudo systemctl enable wifi_full.service
+
 # Create the service that automatically starts the pegasus code on boot
 sudo cp services/pegasus.service /etc/systemd/system/pegasus.service
 sudo systemctl daemon-reload
