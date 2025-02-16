d30e207cb39b5c71fe4898e2e61f6b081b96ad2d
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2025-02-16 18:26:47 +0000

    feat: services to auto-start pegasus

---
+++ pegasus/src/pegasus_drone/scripts/config.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Create the service that automatically puts the jetson in the max power mode every time it boots
+#sudo cp jetson_clocks.service /etc/systemd/system/jetson_clocks.service
+#sudo systemctl daemon-reload
+#sudo systemctl enable jetson_clocks.service
+
+# Create the service that automatically starts the pegasus code on boot
+sudo cp pegasus.service /etc/systemd/system/pegasus.service
+sudo systemctl daemon-reload
+sudo systemctl enable pegasus.service
